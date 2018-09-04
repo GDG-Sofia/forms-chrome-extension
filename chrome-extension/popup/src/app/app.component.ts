df37022efd9d10fe7ff63df65a46f01d450b5520
Slavcho Slavchev <slavsan.mail@gmail.com>
2018-09-04 18:45:06 +0300

    Can open settings page from popup

--- chrome-extension/popup/src/app/app.component.ts
+++ chrome-extension/popup/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var chrome: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chrome-extension';
+
+  openSettings() {
+    chrome.tabs.create({
+      url: chrome.runtime.getURL('settings/index.html')
+    });
+  }
 }
