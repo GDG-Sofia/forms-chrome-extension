595a8867c5ffa247ce4a2884084c3d147ba6d33e
Slavcho Slavchev <slavsan.mail@gmail.com>
2018-09-05 15:26:21 +0300

    step 2: make test pass

--- chrome-extension/content/src/FormInteractor.ts
+++ chrome-extension/content/src/FormInteractor.ts
@@ -10,6 +10,12 @@ class FormInteractor {
   }
 
   handler (request, sender, sendResponse) {
+    const forms = this.doc.querySelectorAll('form');
+
+    if (!forms.length) {
+      return sendResponse({ forms: [] });
+    }
+
     const name = this.doc.querySelector('form').querySelector('input').getAttribute('name');
     const response = {
       forms: [
